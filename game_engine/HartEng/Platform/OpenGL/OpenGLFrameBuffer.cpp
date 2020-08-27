a9dd1f331bdb1b4e3da10dfacaf3deebb52e2a3e
Mark <j18r1l@gmail.com>
2020-08-27 19:30:18 +0300

    framebuffer bugfix

--- game_engine/HartEng/Platform/OpenGL/OpenGLFrameBuffer.cpp
+++ game_engine/HartEng/Platform/OpenGL/OpenGLFrameBuffer.cpp
@@ -4,6 +4,8 @@
 #include "HartEng/Core.h"
 #include "HartEng/Log.h"
 
+#include <glad/glad.h>
+
 namespace HE
 {
     OpenGLFrameBuffer::OpenGLFrameBuffer(const FrameBufferSpecification& spec):
@@ -23,22 +25,21 @@ namespace HE
     {
         HE_PROFILE_FUNCTION();
 
-        glCreateFramebuffers(1, &m_RendererID);
+        glGenFramebuffers(1, &m_RendererID);
         glBindFramebuffer(GL_FRAMEBUFFER, m_RendererID);
 
-        glCreateTextures(GL_TEXTURE_2D, 1, &m_ColorAttachment);
+        glGenTextures(1, &m_ColorAttachment);
         glBindTexture(GL_TEXTURE_2D, m_ColorAttachment);
-        glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, m_Specification.Width, m_Specification.Height);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,  m_Specification.Width, m_Specification.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_ColorAttachment, 0);
 
-        glCreateTextures(GL_TEXTURE_2D, 1, &m_DepthAttachment);
+        glGenTextures(1, &m_DepthAttachment);
         glBindTexture(GL_TEXTURE_2D, m_DepthAttachment);
-        glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, m_Specification.Width, m_Specification.Height);
-        //glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, m_Specification.Width, m_Specification.Height, GL_DEPTH24_STENCIL8, GL_UNSIGNED_BYTE, nullptr);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8,  m_Specification.Width, m_Specification.Height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, nullptr);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, m_DepthAttachment, 0);
-
         HE_CORE_ASSERT(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is incomplete!");
 
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
